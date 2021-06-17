b6791db80d3225e9a2e9f443a67bd6e9d22a6222
Sacha <sachacollev@gmail.com>
2021-06-17 16:06:48 +0200

    Routes/Controllers

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,28 +1,57 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Restaurateur;
+use App\Category;
+use App\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('auth');
+      $this->middleware('auth');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+      return view('home');
     }
+    public function loggedPage() {
+      return view('pages.logged-in');
+    }
+    public function create(){
+    return view('pages.create');
+  }
+  public function store(Request $request){
+    $validate = $request -> validate($this -> getValidate());
+    $restaurateur = Restaurateur::create($validate);
+    return redirect() -> route('restaurateur', $restaurateur -> id);
+  }
+    public function edit($id){
+      $restaurateur = Restaurateur::findOrFail($id);
+      $product = Product::all();
+      $category=Category::all();
+      return view('pages.edit-restaurateur', compact(
+        'restaurateur',
+        'product',
+        'category'
+      ));
+    }
+    public function update(Request $request, $id){
+      $validate = $request -> validate($this -> getValidate());
+      $restaurateur = Restaurateur::findOrFail($id);
+      $restaurateur -> update($validate);
+
+      $restaurateur -> category() -> associate($request -> category_id);
+      $restaurateur -> save();
+      $restaurateur -> products() -> sync($request -> product_id);
+
+      return redirect() -> route('restaurateur');
+    }
+    public function delete($id){
+    $restaurateur = Restaurateur::findOrFail($id);
+    $restaurateur -> deleted = true;
+    $restaurateur -> save();
+    return redirect() -> route('restaurateur');
+  }
 }
