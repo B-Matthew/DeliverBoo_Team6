b77ebb6c5e2d013ff725197f0da46805fa33f9e5
Alessandro Venti <alessandro.venti@icloud.com>
2021-06-21 16:24:28 +0200

    pagina ristoranti personali creata

--- resources/views/pages/dashBoard.blade.php
+++ resources/views/pages/dashBoard.blade.php
@@ -1,82 +1,91 @@
 @extends('layouts.main-layout')
 @section('content')
- <div class="show_div">
-   <div class="">
-     <h1>Nuovo Ristorante</h1>
-   </div>
+<main id="dashboard">
 
-   <h1>Benvenuto {{$user->name}} ID : {{$user->id}}</h1>
-   <h2>I Tuoi Ristoranti:</h2>
-   <ul>
-     @foreach ($user -> restaurants as $u)
-     <li>
-       {{$u -> name}} 
-        @foreach ($u -> categories as $category)
-        {{$category-> name}}
-        @endforeach
-      </li>
-      @endforeach
-    </ul>
-    
-    <form method="POST" action="{{ route('store-restaurant') }}" enctype="multipart/form-data"> 
- 
-      @csrf
-      @method('POST')
- 
-      <div class="">
-          <label class="" for="name"><h4>Nome Ristorante</h4></label>
-          <div class="">
-            <input type="text" class="" id="name" name="name" placeholder="Inserisci il nome..">
-          </div>
-      </div>
-      <div class="">
-          <label class="" for="city"><h4>Città</h4></label>
-          <div class="">
-            <input type="text" class="" id="città" name="city" placeholder="Inserisci la città..">
-          </div>
-      </div>
-      <div class="">
-          <label class="" for="address"><h4>Address</h4></label>
-          <div class="">
-            <input type="text" class="" id="address" name="address" placeholder="Inserisci l'indirizzo..">
-          </div>
-      </div>
-       <div class="">
-           <label class="" for="telephone"><h4>Telephone</h4></label>
-           <div class="">
-             <input type="number" class="" id="telephone" name="telephone" placeholder="Inserisci il numero..">
-           </div>
-       </div>
-       <div class="">
-           <label class="" for="pIva"><h4>Partita Iva</h4></label>
-           <div class="">
-             <input type="number" class="" id="pIva" name="pIva" placeholder="Inserisci la partita Iva..">
-           </div>
-       </div>
-       <div class="">
-           <label class="" for="img"><h4>Immagine del tuo ristorante</h4></label>
-           <div class="">
-             <input type="file" name="img">
-           </div>
-       </div>
-       <div>
-         <ul>
-           @foreach ($categories as $category)
-           <li>
-             <label for="category">{{$category->name}}</label>
-             <input type="checkbox" name="category_id[]" value="{{$category -> id}}">
-           </li>
-           @endforeach
-         </ul>
-       </div>
-       <button type="submit" class="">Submit</button>
-    </form>
-  </div>
- @endsection
-           
-            
-               
-          
-         
+     <section class="welcome">
+          <h1>Bentornato, {{$user->name}}</h1>
+     </section>
    
+     <section class="restaurants">
+          <h3>I tuoi ristoranti</h3>
+          <ul>
+               @foreach ($user -> restaurants as $u)
+               <li>
+                    <img src="{{asset('storage/img/carne3.png')}}" alt="">
+                    <span>{{$u -> name}}</span>
+               </li>
+               @endforeach
+          </ul>
+     </section>
 
+     <section class="create">
+          <h2>Hai una nuova attività?</h2>
+      
+          <form method="POST" action="{{ route('store-restaurant') }}" enctype="multipart/form-data"> 
+               @csrf
+               @method('POST')
+              <fieldset>
+                    <legend>Aggiungi ristorante</legend>
+                    <div>
+                         <label class="" for="name">
+                         <h4>Nome Ristorante</h4>
+                         </label>
+                         <input type="text" class="" id="name" name="name" placeholder="Inserisci il nome..">
+                    </div>
+      
+                    <div>
+                         <label class="" for="city">
+                              <h4>Città</h4>
+                         </label>
+                         <input type="text" class="" id="città" name="city" placeholder="Inserisci la città..">
+                    </div>
+          
+                    <div>
+                         <label class="" for="address">
+                              <h4>Indirizzo</h4>
+                         </label>
+                         <input type="text" class="" id="address" name="address" placeholder="Inserisci l'indirizzo..">
+                    </div>
+      
+                    <div>
+                         <label class="" for="telephone">
+                              <h4>Recapito telefonico</h4>
+                         </label>
+                         <input type="number" class="" id="telephone" name="telephone" placeholder="Inserisci il numero..">
+                    </div>
+
+                    <div>
+                         <label class="" for="pIva">
+                              <h4>Partita Iva</h4>
+                         </label>
+                         <input type="text" class="" id="pIva" name="pIva" placeholder="Inserisci la partita Iva..">
+                    </div>
+
+                    <div>
+                         <label class="" for="img">
+                              <h4>Immagine del tuo ristorante</h4>
+                         </label>
+                         <input type="file" name="img">
+                    </div>
+
+                    <ul>
+                         @foreach ($categories as $category)
+                         <li>
+                              <input type="checkbox" name="category_id[]" value="{{$category -> id}}">
+                              <label for="category">{{$category->name}}</label>
+                         </li>
+                         @endforeach
+                    </ul>
+          
+                    <div class="button">
+                         <button type="submit">
+                         Aggiungi
+                         </button>
+                    </div>
+              </fieldset>
+          </form>
+     </section>
+
+</main>
+    
+@endsection
