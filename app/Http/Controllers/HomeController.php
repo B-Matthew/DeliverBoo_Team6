84d3b2e04bab2281a12483b0d60f3bae8f11908c
Matthew <matsgeco92@hotmail.it>
2021-06-25 15:29:28 +0200

    asd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,10 +14,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-  public function __construct() 
-  {
-    $this->middleware('auth');
-  }
+  // public function __construct() 
+  // {
+  //   $this->middleware('auth');
+  // }
 
   // Dashboard del ristoratore
   public function dashBoard() 
