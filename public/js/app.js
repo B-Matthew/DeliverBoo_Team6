1c9c0ef23cce9ef03f6154ccbab56d6d09fce2bd
nunzio <chiefa.nunzio@gmail.com>
2021-06-29 17:30:46 +0200

    dashboard asfaltata

--- public/js/app.js
+++ public/js/app.js
@@ -449,12 +449,11 @@ __webpack_require__.r(__webpack_exports__);
       }
 
       console.log(filteredCategory);
+      console.log(this.typeOfFoods);
       return filteredCategory;
     }
   },
-  mounted: function mounted() {
-    console.log(this.restaurants);
-    console.log(this.categories);
+  mounted: function mounted() {//   console.log(this.categories);
   }
 });
 
@@ -14038,8 +14037,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/alessandroventi/Desktop/Esercizi boolean/DeliverBoo_Team6/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/alessandroventi/Desktop/Esercizi boolean/DeliverBoo_Team6/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\DeliverBoo_Team6\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\DeliverBoo_Team6\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
