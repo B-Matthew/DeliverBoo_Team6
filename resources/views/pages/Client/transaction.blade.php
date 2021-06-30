2393d96cce114dc0ad24e46240ebfb4c4047014e
nunzio <chiefa.nunzio@gmail.com>
2021-06-30 22:15:08 +0200

    Auto stash before merge of "master" and "origin/master"

--- resources/views/pages/Client/transaction.blade.php
+++ resources/views/pages/Client/transaction.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.main-layout')
 @section('content')
-    <main>
-        <h2 style="padding: 300px 0;">Ciao {{$transaction}}</h2>    
+    <main id="transaction" >
+        {{-- <h2 style="padding: 300px 0;">Ciao {{$transaction}}</h2>    --}}
+        <div>
+            <h2>PERFETTO LA TUA TRANSAZIONE É ANDATA A BUON FINE</h2>
+            
+            <h3>id transazione : {{$transaction}}</h3>
+
+            <h2>IL TUO ORDINE É IN PREPARAZIONE</h2>
+        </div>    
     </main>   
-    @endsection
-        
+@endsection   
