2f12c0f9e31db1ab063ede73b4fe1cf7b70583cf
Alessandro Venti <alessandro.venti@icloud.com>
2021-06-30 19:44:26 +0200

    Creato hamburger menu al click, fatto mediaquery 998-768

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <header>
      <ul>
-          <li>
+          <li class="hide">
                <a href="{{route('infoWebPage')}}">
                     Chi siamo
                </a>
@@ -8,7 +8,7 @@
      
 
      
-          <li>
+          <li class="hide">
                <a href="{{route('faq')}}">
                     Faq
                </a>
@@ -24,7 +24,7 @@
      
 
      
-          <li>
+          <li class="hide">
                @auth
                    <a href="{{ route('dashBoard') }}">{{Auth::user() -> name}}</a>
                <li>
@@ -40,14 +40,50 @@
      
 
      
-          <li>
+          <li class="hide">
                <a href="{{ route('register') }}">
                Registrati
                @endauth
                </a>
           </li>
+
+          <hamburger-component>
+               <li>
+                    <a href="{{route('infoWebPage')}}">
+                         Chi siamo
+                    </a>
+               </li>
+
+               <li>
+                    <a href="{{route('faq')}}">
+                         Faq
+                    </a>
+               </li>
+
+               <li>
+                    @auth
+                        <a href="{{ route('dashBoard') }}">{{Auth::user() -> name}}</a>
+                    <li>
+                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                    </li>
+                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                         @csrf
+                         </form>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+     
+               </li>
+
+               <li>
+                    <a href="{{ route('register') }}">
+                    Registrati
+                    @endauth
+                    </a>
+               </li>
+          </hamburger-component>
      </ul>
 </header>
+
                
 
               
