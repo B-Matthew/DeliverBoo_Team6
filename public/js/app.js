877272ff17ee20682dd6277ff064d11639ec93f6
Alessandro Venti <alessandro.venti@icloud.com>
2021-07-01 00:38:37 +0200

    Mediaquery homepage completate

--- public/js/app.js
+++ public/js/app.js
@@ -39254,7 +39254,7 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("li", [
+  return _c("li", { staticClass: "hamburger-menu" }, [
     _c(
       "button",
       {
