a5a099b83e821c9cdc56181dde175b2238dcefb1
nunzio <chiefa.nunzio@gmail.com>
2021-07-03 15:47:12 +0200

    nex step

--- resources/views/pages/Client/transaction.blade.php
+++ resources/views/pages/Client/transaction.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.main-layout')
 @section('content')
     <main id="transaction" >
-        {{-- <h2 style="padding: 300px 0;">Ciao {{$transaction}}</h2>    --}}
+        
         <div>
             <h2>LA TUA TRANSAZIONE É ANDATA A BUON FINE</h2>
             
             <h3>id transazione : {{$transaction}}</h3>
+
+            <ul>
+                <li></li>   
+            </ul>
 
             <h2>IL TUO ORDINE É IN PREPARAZIONE</h2>
         </div>       
