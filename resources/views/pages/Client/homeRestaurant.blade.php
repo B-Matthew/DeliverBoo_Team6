df654b4228539059b3ad1b736b5de60e6c4546ad
Sacha <sachacollev@gmail.com>
2021-07-05 16:57:51 +0200

    HomeRestaurant

--- resources/views/pages/Client/homeRestaurant.blade.php
+++ resources/views/pages/Client/homeRestaurant.blade.php
@@ -2,38 +2,42 @@
 @section('content')
 <main id="home-restaurant">
     <section class="jumbotron">
+      <div class="jumb">
 
         <h1>{{ $restaurant -> name }}</h1>
 
-        <div>   
-            <i class="far fa-star"></i>   
-            <i class="far fa-star"></i>   
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>
-            <i class="far fa-star"></i>   
-            <span>recensioni?</span>
-        </div>
+          <div class="txt-jumb">
 
-        <div>
-            @foreach($restaurant -> categories as $category)
+            <div>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+              <i class="far fa-star"></i>
+            </div>
+
+
+            <div>
+              @foreach($restaurant -> categories as $category)
                 <span> {{ $category -> name }} </span>
-            @endforeach   
-        </div>      
+              @endforeach
+            </div>
 
-        <div>
-            <p>{{ $restaurant -> address }}</p>
+            <div>
+              <p>{{ $restaurant -> address }}</p>
+            </div>
+          </div>
         </div>
 
     </section>
 
-    <homerestaurant-component 
-    :antipasti="{{$antipasti}}" 
-    :primi="{{$primi}}" :secondi="{{$secondi}}" 
-    checkout="{{route('checkout')}}" 
+    <homerestaurant-component
+    :antipasti="{{$antipasti}}"
+    :primi="{{$primi}}" :secondi="{{$secondi}}"
+    checkout="{{route('checkout')}}"
     :dolci="{{$dolci}}"
     >
-    
-</homerestaurant-component>    
-</main>   
+
+</homerestaurant-component>
+</main>
 @endsection
-    
